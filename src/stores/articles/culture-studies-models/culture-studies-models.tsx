a9c413fcb1a5dd6a9597fa769be63b331d066310
Vivek Maharajh <vivmaha@live.com>
2016-12-27 13:37:44 -0800

    added a quote

--- src/stores/articles/culture-studies-models/culture-studies-models.tsx
+++ src/stores/articles/culture-studies-models/culture-studies-models.tsx
@@ -8,6 +8,10 @@ class Article implements IArticle {
     author = 'Vivek Maharajh';
     introduction = <p>The summaries of three papers that explore the study of culture, and the models that emerge from it.</p>;
     sections = [
+        <section>            
+            <blockquote>“Your company's culture and your company's brand are really just two sides of the same coin. The brand may lag the culture at first, but eventually it will catch up. Your culture is your brand.”</blockquote>
+            <span> — Tony Hsei, Zappos</span>
+        </section>,
         <section>
             <h1>Quesenbery, W., &amp; Szuc, D. (2012). Global UX: Design and research in a connected world. Waltham, MA: Morgan Kaufmann (Chaper 3)</h1>
             <p>Culture is best understood comparatively.</p>
