dcf4b660d8783840c37e0ed213e4a71556207dd5
Vivek Maharajh <vivmaha@live.com>
2017-02-17 20:40:14 -0800

    scroll to top on navigate, except on back

--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,28 @@ import { Article } from "./views/Article";
 
 import "./index.scss";
 
+var history = ReactRouter.browserHistory;
+
+history.listen(location => {
+  // Use setTimeout to make sure this runs after React Router's own listener
+  setTimeout(() => {
+    // Keep default behavior of restoring scroll position when user:
+    // - clicked back button
+    // - clicked on a link that programmatically calls `history.goBack()`
+    // - manually changed the URL in the address bar (here we might want
+    // to scroll to top, but we can't differentiate it from the others)
+    if (location.action === 'POP') {
+      return;
+    }
+    // In all other cases, scroll to top
+    window.scrollTo(0, 0);
+  });
+});
+
 ReactDOM.render(
     (
         /* TODO: When you deploy this to a real server, you have to setup URL redirection, so that everythign gets redirected to index.html */
-        <ReactRouter.Router history={ReactRouter.browserHistory}>
+        <ReactRouter.Router history={history}>
             <ReactRouter.Route path="/" component={Home} />
             <ReactRouter.Route path="/article/:articleId" component={Article} />
         </ReactRouter.Router>
