344e21f59d949038d7dbdcf103d96e116a17faf7
Vivek Maharajh <vivmaha@live.com>
2017-02-19 20:24:03 -0800

    unregister scroll event handler when home is unmounted

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -18,17 +18,17 @@ export class Home extends React.Component<Props, State> {
         super(props);
         this.state = new State();
         this.state.Articles = [];
+        this.handleScroll = this.handleScroll.bind(this);
     }
 
     public componentDidMount() {
         let store = new ArticleStore();
         this.setState({Articles: store.getArticles()} as State);
-        window.addEventListener('scroll', (ev) => this.handleScroll(ev));
+        window.addEventListener('scroll', this.handleScroll);
     }
 
-    public componentWillUnmount() {
-        // TODO: How to unregister
-        // window.removeEventListener('scroll', this.handleScroll);
+    public componentWillUnmount() {        
+         window.removeEventListener('scroll', this.handleScroll);
     }
 
     handleScroll(event: UIEvent) {        
