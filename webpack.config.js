5991f7b9ca61d7cb31e0ac5f0d23ffb58364ecad
Vivek Maharajh <vivmaha@live.com>
2017-02-25 22:31:10 -0800

    using the production build of react on production

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,24 @@
-var failPlugin = require('webpack-fail-plugin');
+var webpackFailPlugin = require('webpack-fail-plugin');
+var webpack = require('webpack');
+var webpackBundleAnalyzer = require('webpack-bundle-analyzer')
+
+const PRODUCTION = process.env.NODE_ENV === 'production';
+
+var plugins = PRODUCTION ? [
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: JSON.stringify('production')
+        }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        }
+    }),
+] : [
+    // Workaround for https://github.com/webpack/webpack/issues/708
+    webpackFailPlugin
+]
 
 module.exports = {
     devServer: {
@@ -14,10 +34,7 @@ module.exports = {
     output: {
         filename: "./bundle.js",
     },
-    plugins: [
-        // Workaround for https://github.com/webpack/webpack/issues/708
-        failPlugin,
-    ],
+    plugins: plugins,
     resolve: {
         extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
     },
