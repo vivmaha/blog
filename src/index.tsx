a989f674a406587134041e2c470c4d72855b4325
Vivek Maharajh <vivmaha@live.com>
2017-05-05 00:22:30 -0700

    fixed scroll to top bug with react-router

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,15 @@ import { Home } from "./views/Home";
 import { Article } from "./views/Article";
 import { ArticleSet } from "./views/ArticleSet";
 
+import { ScrollToTop } from "./components/ScrollToTop";
+
 import "./index.scss";
 
 render(
     (
-        /**
-         * Since this page handles all routing, the web server has to be configured to 
-         * redirect all page requests to this page instead. This is done in the web.config
-         */
         <BrowserRouter>
           <div>
+            <ScrollToTop></ScrollToTop>
             <Route exact path="/" component={Home} />
             <Route path="/article/:id" component={Article} />
             <Route path="/series/:id" component={ArticleSet} />
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,4 +3,7 @@ const CommonConfig = require('./webpack.common.js');
 
 module.exports = Merge(CommonConfig, {
     devtool: 'cheap-module-eval-source-map',
+    devServer : {
+        historyApiFallback: true
+    }
 });
