12df22dda392cce223db9c6241e14787d3aaf2cf
Vivek Maharajh <vivmaha@live.com>
2017-06-24 21:14:19 -0700

    Breaking change in typescript - void -> {}

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -3,7 +3,7 @@ import { RouteComponentProps, withRouter, RouteProps} from "react-router";
 
 // Scroll restoration based on https://reacttraining.com/react-router/web/guides/scroll-restoration.
 export var ScrollToTop = withRouter<RouteProps>(
-  class ScrollToTopWithoutRouter extends React.Component<RouteComponentProps<any>, void> {
+  class ScrollToTopWithoutRouter extends React.Component<RouteComponentProps<any>, {}> {
     componentDidUpdate(prevProps: Readonly<RouteComponentProps<any>>) {
       if (this.props.location !== prevProps.location) {
         window.scrollTo(0, 0)
