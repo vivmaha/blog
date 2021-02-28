b49589c3aca08006d3db2a2f0294e4063cb836cf
V Maharajh <vmaharajh@tableau.com>
2021-02-27 21:26:53 -0800

    got rid of redux and article contents

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,14 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
-import { Provider } from "react-redux";
 
 import HomeView from "./views/HomeView";
 import { ArticleView } from "./views/ArticleView";
 import ArticleSetView from "./views/ArticleSetView";
-import store from "./redux/Store";
-
 import "./index.scss";
 import { ScrollToTop } from "./components/ScrollToTop";
 
 render(
-  <Provider store={store}>
     <BrowserRouter>
       <ScrollToTop />
       <div>
@@ -20,7 +16,6 @@ render(
         <Route path="/article/:id" component={ArticleView} />
         <Route path="/series/:id" component={ArticleSetView} />
       </div>
-    </BrowserRouter>
-  </Provider>,
+    </BrowserRouter>,
   document.getElementById("content")
 );
