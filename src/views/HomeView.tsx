03e5bd52032fa195dfc103c3c75b10fb1761a297
V Maharajh <vmaharajh@tableau.com>
2021-03-01 21:46:51 -0800

    Added loading experiences

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,24 +1,11 @@
 import * as React from "react";
+import { Series } from "../components/Series";
 
-import { useEffect, useState } from "react";
-import { getSeries } from "../api/get-series";
-import { Series } from "../api/models/series";
-import Home from "../components/Home";
-
-
-
-const HomeView: React.FC = () => {
-  const [series, setSeries] = useState<Series>();
-
-  useEffect(() => {
-    getSeries("home").then(setSeries);
-  }, []);
-
-  if (series === undefined) {
-    // TODO Improve this UX
-    return <div>Loading...</div>;
-  }
-  return <Home series={series} />;
-};
-
-export { HomeView as default };
+export const HomeView: React.FC = () =>
+  <Series
+    id="home"
+    bannerLink={{
+      url: "/article/about",
+      text: "About",
+    }}
+  />
