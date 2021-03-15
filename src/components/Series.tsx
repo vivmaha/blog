93faf04cba48b24dc1f25fd1a3874a7f618c4a16
V Maharajh <vivmaha@gmail.com>
2021-03-15 08:51:04 -0700

    fixed lint errors

--- src/components/Series.tsx
+++ src/components/Series.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+
 import { getSeries } from "../api/get-series";
 
 import { Series as SeriesModel } from "../api/models/series";
@@ -7,30 +7,30 @@ import Articles from "./Articles";
 import { FullPageSpinner } from "./FullPageSpinner";
 
 export type SeriesProps = {
-    id: string;
-    bannerLink: {
-        url: string;
-        text: string;
-    }
-}
+  id: string;
+  bannerLink: {
+    url: string;
+    text: string;
+  };
+};
 
-export const Series: React.FC<SeriesProps> = ({id, bannerLink}) => {
-    const [series, setSeries] = useState<SeriesModel>();
+export const Series: React.FC<SeriesProps> = ({ id, bannerLink }) => {
+  const [series, setSeries] = useState<SeriesModel>();
 
-    useEffect(() => {
-      getSeries(id).then(setSeries);
-    }, [id]);
-  
-    if (series === undefined) {
-      return <FullPageSpinner />
-    }
-    return (
-      <Articles
-        articles={series.articles}
-        introduction={series.introduction}
-        bannerTitle={series.title}
-        bannerLink={bannerLink}
-        backgroundImageUrl={series.backgroundImageUrl}
-      />
-    );
-}
+  useEffect(() => {
+    getSeries(id).then(setSeries);
+  }, [id]);
+
+  if (series === undefined) {
+    return <FullPageSpinner />;
+  }
+  return (
+    <Articles
+      articles={series.articles}
+      introduction={series.introduction}
+      bannerTitle={series.title}
+      bannerLink={bannerLink}
+      backgroundImageUrl={series.backgroundImageUrl}
+    />
+  );
+};
