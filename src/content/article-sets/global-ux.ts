ed1a8d10162b509b295698c086932c7dd0c8aee0
V Maharajh <vivmaha@gmail.com>
2021-03-15 08:51:02 -0700

    changed bannerContent to a simple string

--- src/content/article-sets/global-ux.ts
+++ src/content/article-sets/global-ux.ts
@@ -7,15 +7,7 @@ export default {
   title: "Global UX",
   backgroundImageUrl:
     "https://notesbyvmedia.blob.core.windows.net/images/car-on-map-web-optimized.jpg",
-  introduction: {
-    type: "html",
-    content: `
-        <p class="force-margin-top">
-            <span>A literature review.</span>
-            <br/>
-            <br/>
-            <span>This was part of my coursework at the University of Washington. Specifically, HCDE 512 with Manuela Noske.</span>
-        </p>
-        `,
-  } as ArticleContentHtml,
+  introduction: `A literature review.
+
+This was part of my coursework at the University of Washington. Specifically, HCDE 512 with Manuela Noske.`,
 } as Series;
