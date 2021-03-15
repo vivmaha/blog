23b6e9b4fc4dd3d6e8c89c1beafcf0171a55d6c6
V Maharajh <vivmaha@gmail.com>
2021-03-15 08:51:01 -0700

    got rid of redux and article contents

--- src/redux/Store.ts
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunkMiddleware from "redux-thunk";
-import reducer from "./Reducer";
-
-const store = createStore(reducer, applyMiddleware(thunkMiddleware));
-
-export { store as default };
